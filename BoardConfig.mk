f8d93d4dc6e343b9a82f746123908ca2e0d8f436
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-03-28 06:46:13 +0700

    Infinix-X695C: Inherit from mt6785-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@ DEVICE_PATH := device/infinix/Infinix-X695C
 # Inherit from mt6785-common
 include device/transsion/mt6785-common/BoardConfigCommon.mk
 
-# TWRP Configs
-TW_DEVICE_VERSION := Infinix-X695C | ExcaliburXD
-
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_Infinix-X695C
 TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X695C
