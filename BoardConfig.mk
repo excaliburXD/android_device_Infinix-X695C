7dfd20be354dd98ff4e1802fe31add50b35cda0f
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-03-31 11:55:45 +0700

    Infinix-X695C: Inherit from mt6785-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,12 @@ DEVICE_PATH := device/infinix/Infinix-X695C
 # Inherit from mt6785-common
 include device/transsion/mt6785-common/BoardConfigCommon.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := X695C,Infinix-X695C
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_Infinix-X695C
 TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X695C
+
+# Maintainer
+TW_DEVICE_VERSION := Infinix Note 10 Pro NFC | ExcaliburXD
