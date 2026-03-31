c5cfabaaf4ee906040fdd71372d0d9c0c147de10
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-03-31 12:03:12 +0700

    Infinix-X695C: Inherit from mt6785-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2026 The TWRP Open Source Project
-# Copyright (C) 2026 The TWRP Open Source Project
+# Copyright (C) 2026 The OFRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +18,9 @@ DEVICE_PATH := device/infinix/Infinix-X695C
 
 # Inherit from mt6785-common
 include device/transsion/mt6785-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := X695C,Infinix-X695C
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_Infinix-X695C
